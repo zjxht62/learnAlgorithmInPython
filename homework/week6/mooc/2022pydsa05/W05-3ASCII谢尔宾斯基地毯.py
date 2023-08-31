1a28a1a1a9139b081eb17ce0d6ea2156dc38790a
ZhaoJixiang <zhaojixiang@zhnrh.com>
2023-08-31 09:53:10 +0800

    [feat]添加第6周作业,谢尔宾斯基地毯

--- homework/week6/mooc/2022pydsa05/W05-3ASCII谢尔宾斯基地毯.py
+++ homework/week6/mooc/2022pydsa05/W05-3ASCII谢尔宾斯基地毯.py
@@ -24,20 +24,26 @@
 # []  [][]  [][]  []
 # [][][][][][][][][]
 
-def sierpinski(degree, s):
-    if degree >= 3:
-        empty = degree//3
-        for i in range(degree):
-            if i in range(empty, empty+empty-1):
-                print('haha')
-                # print(s*empty + ' '*empty + s*empty)
-                print(s*empty + ' '*empty + s*empty)
-            else:
-                print(s*degree)
-        # sierpinski(degree//3, s)
+n = int(input())  # 阶数
+ch = input()  # 构成字符
+blank = " " * len(ch)  # 构成空白
+pic = [[ch for col in range(n)] for row in range(n)]  # 先初始化画板，也就是完整的由符号组成的n阶图形
 
 
+def spski(n, top, left):  # n阶， 左上角的行列数
+    if n == 1:
+        return
+    # 分为三行三列，挖掉中心，其余递归n//3
+    for row in range(3):
+        for col in range(3):
+            if row == 1 and col == 1:  # 挖空
+                for r1 in range(n // 3):
+                    for c1 in range(n // 3):
+                        pic[top + n // 3 + r1][left + n // 3 + c1] = blank
+            else:  # 递归n//3
+                spski(n // 3, top + row * n // 3, left + col * n // 3)
 
-if __name__ == '__main__':
-    # sierpinski(3,'a')
-    print(1 in range(1,2))
+
+spski(n, 0, 0)
+for r in range(n):
+    print("".join(pic[r]))
