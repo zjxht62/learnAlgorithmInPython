8541478919c1e42392bd929f06cc137ec60810f5
ZhaoJixiang <zhaojixiang@zhnrh.com>
2023-09-18 15:40:04 +0800

    [feat]添加博物馆大盗问题老师提供的题解

--- DSA22/homework/week8/class/H3-1_my.py
+++ DSA22/homework/week8/class/H3-1_my.py
@@ -1,3 +1,4 @@
+
 # =========== 1 博物馆大盗问题 ===========
 # 给定一个宝物列表treasureList = [{'w': 2,'v': 3}, {'w': 3,'v': 4}, ...]
 # 注意：每样宝物只有1个。
@@ -11,49 +12,38 @@ def dpMuseumThief(treasureList, maxWeight):
     choosenList = []
 
     # 请在此编写你的代码（可删除pass语句）
-    tr = [None] + treasureList  # 让宝物编码从1开始
-    # 初始化二维表格m[(i,w)]，均为0，None
-    # 表示前i个宝物中，最大重量w的组合，所得到的最大价值
-    # 以及加了哪个宝物得到的这个最大价值
-    # 当i什么都不取，或w上限为0，价值均为0
-    m = {(i, w): [0, None] for i in range(len(tr)) for w in range(maxWeight + 1)}
-    for i in range(1, len(tr)):
-        for w in range(1, maxWeight + 1):
-            if tr[i]['w'] > w:
-                m[i, w][0] = m[i - 1, w][0]
+    table = [[0] * (maxWeight + 1) for i in range(len(treasureList))]
+    for i in range(len(treasureList)):
+        for w in range(maxWeight + 1):
+            if treasureList[i]['w'] > w:
+                table[i][w] = table[i - 1][w]
             else:
                 # 放入当前宝物后的价值
-                v_put = tr[i]['v'] + m[i - 1, w - tr[i]['w']][0]
+                v_put = treasureList[i]['v'] + table[i - 1][w - treasureList[i]['w']]
                 # 不放入当前宝物时的价值
-                v_not_put = m[i - 1, w][0]
-                # table[i][w] = max(v_put, v_not_put)
-                if v_put >= v_not_put:
-                    m[i, w][0] = v_put
-                    m[i, w][1] = tr[i]
-                else:
-                    m[i, w][0] = v_not_put
+                v_not_put = table[i - 1][w]
+                table[i][w] = max(v_put, v_not_put)
+                # if v_put >= v_not_put:
+                #     choosenList.append(treasureList[i])
+                #     table[i][w] = v_put
+                # else:
+                #     table[i][w] = v_not_put
+    # for line in table:
+    #     print(line)
+    maxValue = table[len(treasureList) - 1][maxWeight]
 
-    maxValue = m[i, w][0]
-    while w > 0:
-        if m[i, w][1] is not None:  # 如果装了宝物就输出
-            choosenList.insert(0, m[i, w][1])
-            # 重量减去当前宝物的重量
-            w = w - m[i, w][1]['w']
-        # 考虑上一个宝贝
-        i = i - 1
-
-    # current_max_value = maxValue
-    # current_max_weight = maxWeight
-    # for i in range(len(treasureList) - 1, -1, -1):
-    #     # print(table[i])
-    #     if i > 0:
-    #         if current_max_value != table[i - 1][current_max_weight]:
-    #             choosenList.append(treasureList[i])
-    #             current_max_value = current_max_value - treasureList[i]['v']
-    #             current_max_weight = current_max_weight - treasureList[i]['w']
-    #     elif i == 0:
-    #         if treasureList[i]['w'] <= current_max_weight:
-    #             choosenList.append(treasureList[i])
+    current_max_value = maxValue
+    current_max_weight = maxWeight
+    for i in range(len(treasureList) - 1, -1, -1):
+        # print(table[i])
+        if i > 0:
+            if current_max_value != table[i - 1][current_max_weight]:
+                choosenList.append(treasureList[i])
+                current_max_value = current_max_value - treasureList[i]['v']
+                current_max_weight = current_max_weight - treasureList[i]['w']
+        elif i == 0:
+            if treasureList[i]['w'] <= current_max_weight:
+                choosenList.append(treasureList[i])
 
     # 代码结束
 
@@ -102,6 +92,7 @@ for i in range(len(treasureList)):
     print(maxValue)
     print(choosenList)
 
+
 # 可有多种取法，以下只给出一种符合条件的宝物列表
 # 29
 # [{'w':2, 'v':3}, {'w':4, 'v':8}, {'w':5, 'v':8}, {'w':9, 'v':10}]
@@ -113,3 +104,4 @@ for i in range(len(treasureList)):
 # [{'w': 1, 'v': 2}, {'w': 3, 'v': 5}, {'w': 8, 'v': 13}, {'w': 9, 'v': 15}, {'w': 9, 'v': 16}, {'w': 10, 'v': 16}, {'w': 10, 'v': 17}, {'w': 11, 'v': 18}, {'w': 12, 'v': 19}, {'w': 13, 'v': 21}, {'w': 14, 'v': 22}]
 # 246
 # [{'w': 1, 'v': 2}, {'w': 3, 'v': 4}, {'w': 3, 'v': 5}, {'w': 9, 'v': 15}, {'w': 10, 'v': 17}, {'w': 11, 'v': 18}, {'w': 11, 'v': 19}, {'w': 12, 'v': 20}, {'w': 13, 'v': 21}, {'w': 14, 'v': 23}, {'w': 15, 'v': 24}, {'w': 15, 'v': 25}, {'w': 16, 'v': 26}, {'w': 17, 'v': 27}]
+
