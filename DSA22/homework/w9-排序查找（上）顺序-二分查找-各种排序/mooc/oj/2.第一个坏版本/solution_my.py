22e5abe53873033a821924b3d215284838bec0d9
ZhaoJixiang <zhaojixiang@zhnrh.com>
2024-01-29 17:23:21 +0800

    [feat]添加w9的oj第二题：第一个坏版本题解

--- DSA22/homework/w9-排序查找（上）顺序-二分查找-各种排序/mooc/oj/2.第一个坏版本/solution_my.py
+++ DSA22/homework/w9-排序查找（上）顺序-二分查找-各种排序/mooc/oj/2.第一个坏版本/solution_my.py
@@ -1,27 +1,29 @@
-isBadVersion = 'lambda n:n>=30'
+def isBadVersion(n):
+    if n > 9:
+        return True
+    else:
+        return False
 
-versions = [i for i in range(50)]
-print(versions)
+# versions = [i for i in range(50)]
+# print(versions)
+#
+# bad_versions = list(filter(eval(isBadVersion), versions))
+# print(bad_versions)
 
-bad_versions = list(filter(eval(isBadVersion), versions))
-print(bad_versions)
 
-
-def binary_search(versions, lambda_exp):
-    start = 0
-    end = len(versions) - 1
-    while start <= end:
+def first_bad_version(N):
+    start = 1
+    end = N
+    while start < end:
         mid = (start + end) // 2
-        if lambda_exp == versions[mid]:
-            return True
-        elif lambda_exp < mid:
-            end = mid - 1
+        if isBadVersion(mid):
+            end = mid
         else:
             start = mid + 1
-    return False
+    return start
 
 
-if __name__ == '__main__':
-    print(binary_search([1,2,3,4,6,7,8,9],4))
-
+num = int(input())
+isBadVersion = eval(input())
+print(first_bad_version(num))
 
